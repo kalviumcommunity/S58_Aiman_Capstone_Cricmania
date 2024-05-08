f7a30021d561a0cf9cd101295b660f047d406007
Aimans30 <aiman.singh@kalvium.community>
2024-05-08 12:20:53 +0530

    Used Callback Function

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,6 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${server.address().port}`);
 });
