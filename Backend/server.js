470bb21db6e40ce7e3a60113e8575e1b2938ccb2
Aimans30 <aiman.singh@kalvium.community>
2024-05-23 13:37:32 +0530

    Created PUT Request

--- Backend/server.js
+++ Backend/server.js
@@ -8,18 +8,33 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.post('/register', (req, res) => {
+app.post('/post', (req, res) => {
   const { email, password } = req.body;
 
   if (typeof email !== 'string' || typeof password !== 'string') {
-    return res.status(400).json({ error: 'Email and password must be strings' });
+    return res.status(400).json({ error: 'Invalid Email or Password' });
   }
 
   if (!email.trim() || !password.trim()) {
     return res.status(400).json({ error: 'Email and password are required' });
   }
 
-  res.status(200).json({ message: 'Registered successfully',  });
+  res.status(200).json({ message: 'Registered successfully' });
+});
+
+app.put('/update', (req, res) => {
+  const { email, password } = req.body;
+
+  if (typeof email !== 'string' || typeof password !== 'string') {
+    return res.status(400).json({ error: 'Invalid Email or Password' });
+  }
+
+  if (!email.trim() || !password.trim()) {
+    return res.status(400).json({ error: 'Email and password are required' });
+  }
+
+ 
+  res.status(200).json({ message: 'Updated successfully' });
 });
 
 const server = app.listen(PORT, () => {
